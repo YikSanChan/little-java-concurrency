6e2ce3cf7cb81bd3eac1fc017ccff5ce876897ba
Yik San Chan <evan.chanyiksan@gmail.com>
2020-11-29 17:14:17 -0800

    Improve ArrayBlockingQueue

--- src/littlejava/util/concurrent/ArrayBlockingQueue.java
+++ src/littlejava/util/concurrent/ArrayBlockingQueue.java
@@ -1,39 +1,56 @@
 package littlejava.util.concurrent;
 
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class ArrayBlockingQueue<E> implements BlockingQueue<E> {
 
     private final Object[] items;
     private int takeIndex;
     private int putIndex;
     private int count;
+    private final ReentrantLock lock;
+    private final Condition notEmpty;
+    private final Condition notFull;
 
     public ArrayBlockingQueue(int capacity) {
         if (capacity <= 0)
             throw new IllegalArgumentException();
         items = new Object[capacity];
+        lock = new ReentrantLock();
+        notEmpty = lock.newCondition();
+        notFull = lock.newCondition();
     }
 
     @Override
-    public synchronized void put(E e) throws InterruptedException {
-        while (count == items.length)
-            wait();
-        enqueue(e);
-        notifyAll();
+    public void put(E e) throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == items.length)
+                notFull.await();
+            enqueue(e);
+        } finally {
+            lock.unlock();
+        }
     }
 
     @Override
-    public synchronized E take() throws InterruptedException {
-        while (count == 0)
-            wait();
-        E v = dequeue();
-        notifyAll();
-        return v;
+    public E take() throws InterruptedException {
+        lock.lock();
+        try {
+            while (count == 0)
+                notEmpty.await();
+            return dequeue();
+        } finally {
+            lock.unlock();
+        }
     }
 
     private void enqueue(E e) {
         items[putIndex] = e;
         if (++putIndex == items.length) putIndex = 0;
         count++;
+        notEmpty.signal();
     }
 
     private E dequeue() {
@@ -42,6 +59,7 @@ public class ArrayBlockingQueue<E> implements BlockingQueue<E> {
         items[takeIndex] = null;
         if (++takeIndex == items.length) takeIndex = 0;
         count--;
+        notFull.signal();
         return e;
     }
 }
